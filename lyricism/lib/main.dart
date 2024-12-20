a8a6eb7150ca3240a14d6b439805263248f03237
Ian Nkwocha <11nkwochaian@gmail.com>
2024-12-20 13:49:19 +0000

    Rebuild

--- lyricism/lib/main.dart
+++ lyricism/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class QuotesGridApp extends StatelessWidget {
-  const QuotesGridApp({super.key});
+  const QuotesGridApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,82 +19,82 @@ class QuotesGridApp extends StatelessWidget {
 }
 
 class QuotesGridPage extends StatelessWidget {
-  const QuotesGridPage({super.key});
+  const QuotesGridPage({Key? key}) : super(key: key);
 
   final List<Map<String, String>> quotes = const [
     {
-      'tagline': 'A message to Saminder',
-      'lyrics': 'Ain\'t nobody like you in this bitch',
-      'message': 'I\'ve not met many people willing to roast me within the first week of us meeting (an a racially motivated roast no less).\nKinda wild when I think about it, but I knew off-rip that we\'d be cool.'
-      'While I\'ll never forgive you for exposing my scamming activities, I do appreciate how much of a good friend you\'ve been to me.',
-      'spotifyUrl': 'https://open.spotify.com/track/3BUWNzPWz2mDbptZmGEXpB?si=e8631b3ec3784188',
-      'from': ''
-    },
-    {
-      'tagline': 'In appreciation of your one of your greatest assets',
-      'lyrics': 'How you feeling, everybody? Hope you had a nice day.',
-      'message': 'Never thought I would hear someone be offered a job because of their smile. No matter how tough life gets, keep smiling - it\'ll get you far.',
-      'spotifyUrl': 'https://open.spotify.com/track/5SNPmbE3OcIVUlzQj9M32N?si=60c9b8fd2adb40e4',
-      'from': ''      
-    },
-    {
-      'tagline': 'In regard for your committment to being on time',
-      'lyrics': 'We gotta go',
-      'message': 'It\'s actually impressive how you manage to be the last person ready for the gym every week and yet you\'ll always pretend you\'ve '
-      'been waiting. I\'m sharing this song with you as it will be the alarm for you to get ready for the gym. Maybe now you\'ll actually be ready on time.',
-      'spotifyUrl': 'https://open.spotify.com/track/65X7IUg2rPy57wK7mnVNqS?si=123154a919c9418d',
-      'from': ''
-    },
-    {
-      'tagline': 'In acknowledgement of your loyalty',
-      'lyrics': 'Damn I miss my 🥷🏿, pour out a little liquor.',
-      'message': 'I remember you saying that you\'d help me hide a body if I asked. I consider this a great honour.'
-      'Just know that I\'d also expect you to take the fall for it if we got caught. Dw I\'d pour out a cold one in memory of you :)',
-      'spotifyUrl': 'https://open.spotify.com/track/63gHnuI9MzZA4lLob8t9IY?si=4382c3ad5f0249a1',
-      'from': ''
-    },
-    {
-      'tagline': 'In admiration of your ability to take a joke',
-      'lyrics': 'Sometimes we laugh and sometimes we cry',
-      'message': 'Thanks for letting me cook you every time you pull one of your 4 faces 😬😁😲😐, or come to work looking like Trossard, or catch you with an umbrella.\nIt won\'t stop but I know you can handle it.',
-      'spotifyUrl': 'https://open.spotify.com/track/2SAqBLGA283SUiwJ3xOUVI?si=3eccbd790eb34373',
-      'from': ''      
-    },
-    {
-      'tagline': 'In respect of your trustworthy-ness',
-      'lyrics': 'I got a million friends, I only trust \'bout four or five of \'em',
-      'message': 'It speaks to your character that many of us tell you our secrets and they never get around.',
-      'spotifyUrl': 'https://open.spotify.com/track/4rkChovyQKSVPBv87qQnIZ?si=8928ade5d1ec4cf9',
-      'from': ''      
-    },
-    {
-      'tagline': 'In recognition of your tenacity',
-      'lyrics': 'Life hard but I go harder.',
-      'message': 'We see how hard you\'ve grinding. Recognise much you\'ve grown this year',
-      'spotifyUrl': 'https://open.spotify.com/track/7B09ZewAliWo7Y3TdIA7g1?si=461f0199ce5d40e5',
-      'from': ''      
-    },
-    {
-      'tagline': 'In tribute to your promo',
-      'lyrics': 'Every day I kill it like it try to beat my mother.',
-      'message': '',
-      'spotifyUrl': 'https://open.spotify.com/track/6f0WsFJtn4Ptcil4koEfQP?si=65f3af3405c24111',
-      'from': ''      
-    },
-    {
-      'tagline': 'In validation of your sorrows',
-      'lyrics': 'How do you fight the feelin\'',
-      'message': 'A great thing about this year being over is that you won\'t have to relive any of the bad days again.\nJust keep looking forward, put on your favourite artist and vibe.',
-      'spotifyUrl': 'https://open.spotify.com/track/3YchUDysu9jfzeSRG8st77?si=40b7348682a0418a',
-      'from': ''      
-    },
-    {
-      'tagline': 'In celebration of your birthday',
-      'lyrics': 'Pree more life',
-      'message': 'I hope you enjoyed the songs, new and old. Cheers to many more years of friendship!',
-      'spotifyUrl': 'https://open.spotify.com/track/73e0LixjWj3e8prfptVeGD?si=9fc96a966d6e4850',
-      'from': ''      
-    }
+        'tagline': 'A message to Saminder',
+        'lyrics': 'Ain\'t nobody like you in this bitch',
+        'message': 'I\'ve not met many people willing to roast me within the first week of us meeting (an a racially motivated roast no less).\nKinda wild when I think about it, but I knew off-rip that we\'d be cool.\n'
+        'While I\'ll never forgive you for exposing my scamming activities, I do appreciate how much of a good friend you\'ve been to me.',
+        'spotifyUrl': 'https://open.spotify.com/track/3BUWNzPWz2mDbptZmGEXpB?si=e8631b3ec3784188',
+        'from': ''
+      },
+      {
+        'tagline': 'In appreciation of your one of your greatest assets',
+        'lyrics': 'How you feeling, everybody? Hope you had a nice day.',
+        'message': 'Never thought I would hear someone be offered a job because of their smile. No matter how tough life gets, keep smiling - it\'ll get you far.',
+        'spotifyUrl': 'https://open.spotify.com/track/5SNPmbE3OcIVUlzQj9M32N?si=60c9b8fd2adb40e4',
+        'from': ''      
+      },
+      {
+        'tagline': 'In regard for your committment to being on time',
+        'lyrics': 'We gotta go',
+        'message': 'It\'s actually impressive how you manage to be the last person ready for the gym every week and yet you\'ll always pretend you\'ve '
+        'been waiting. This song will be your alarm to pack up so we can go gym. Hopefully you\'ll actually be ready on time.',
+        'spotifyUrl': 'https://open.spotify.com/track/65X7IUg2rPy57wK7mnVNqS?si=123154a919c9418d',
+        'from': ''
+      },
+      {
+        'tagline': 'In acknowledgement of your loyalty',
+        'lyrics': 'Damn I miss my 🥷🏿, pour out a little liquor.',
+        'message': 'I remember you saying that you\'d help me hide a body if I asked.\n'
+        'Just know that I\'d also expect you to take the fall for it if we got caught.',
+        'spotifyUrl': 'https://open.spotify.com/track/63gHnuI9MzZA4lLob8t9IY?si=4382c3ad5f0249a1',
+        'from': ''
+      },
+      {
+        'tagline': 'In admiration of your ability to take a joke',
+        'lyrics': 'Sometimes we laugh and sometimes we cry',
+        'message': 'Thanks for letting us roast you every time you pull one of your 4 faces 😬😁😲😐, or come to work looking like Trossard, or catch you with an umbrella.\nIt won\'t stop but we know you can handle it.',
+        'spotifyUrl': 'https://open.spotify.com/track/2SAqBLGA283SUiwJ3xOUVI?si=3eccbd790eb34373',
+        'from': ''      
+      },
+      {
+        'tagline': 'In respect of your trustworthy-ness',
+        'lyrics': 'I got a million friends, I only trust \'bout four or five of \'em',
+        'message': 'It speaks to your character that many of us tell you our secrets and they never get around.\nThanks for being so trustworthy.',
+        'spotifyUrl': 'https://open.spotify.com/track/4rkChovyQKSVPBv87qQnIZ?si=8928ade5d1ec4cf9',
+        'from': ''      
+      },
+      {
+        'tagline': 'In recognition of your tenacity',
+        'lyrics': 'Life hard but I go harder.',
+        'message': 'We see how hard you\'ve grinding. Recognise much you\'ve grown this year while you listen to this one.',
+        'spotifyUrl': 'https://open.spotify.com/track/7B09ZewAliWo7Y3TdIA7g1?si=461f0199ce5d40e5',
+        'from': ''      
+      },
+      {
+        'tagline': 'In tribute to your promo',
+        'lyrics': 'Every day I kill it like it try to beat my mother.',
+        'message': 'Congrats on winning the hunger games. It was about time your hard work was recognised with a promotion.',
+        'spotifyUrl': 'https://open.spotify.com/track/6f0WsFJtn4Ptcil4koEfQP?si=65f3af3405c24111',
+        'from': ''      
+      },
+      {
+        'tagline': 'In validation of your sorrows',
+        'lyrics': 'How do you fight the feelin\'',
+        'message': 'A great thing about this year being over is that you won\'t have to relive any of the bad days again.\nJust keep looking forward, put on your favourite artist and vibe.',
+        'spotifyUrl': 'https://open.spotify.com/track/3YchUDysu9jfzeSRG8st77?si=40b7348682a0418a',
+        'from': ''      
+      },
+      {
+        'tagline': 'In celebration of your birthday',
+        'lyrics': 'Pree more life',
+        'message': 'I hope you enjoyed the songs, new and old. Cheers to many more years of friendship!',
+        'spotifyUrl': 'https://open.spotify.com/track/73e0LixjWj3e8prfptVeGD?si=9fc96a966d6e4850',
+        'from': ''      
+      }
   ];
 
   @override
@@ -122,7 +122,7 @@ class QuotesGridPage extends StatelessWidget {
                 );
               },
               child: Card(
-                color: Colors.deepPurple[50],
+                color: Colors.purple[50],
                 child: Center(
                   child: Text(
                     quote['tagline']!,
@@ -142,7 +142,7 @@ class QuotesGridPage extends StatelessWidget {
 class QuoteDetailPage extends StatelessWidget {
   final Map<String, String> quote;
 
-  const QuoteDetailPage({super.key, required this.quote});
+  const QuoteDetailPage({Key? key, required this.quote}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -154,13 +154,13 @@ class QuoteDetailPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '"${quote['lyrics']!}"',
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              '"${quote['lyrics']}"',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
             ),
             const SizedBox(height: 16),
             Text(
-              'Context: ${quote['message']}',
-              style: const TextStyle(fontSize: 16),
+              '${quote['message']}',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
             ),
             const Spacer(),
             Center(
